0b6f7e1971568190c96c326f3a9909c40d59ad31
AaronSunil <72694245+AaronSunil@users.noreply.github.com>
2021-02-26 12:01:23 +0530

    Add files via upload

--- Mango.js
+++ Mango.js
@@ -1,10 +1,12 @@
 class Mango {
-    constructor(x, y) {
+    constructor(x, y, width, height) {
       var options = {
           isStatic:true,
           'restitution':0,
           'friction':1
       }
+      this.width = width;
+      this.height = height;
       this.body = Bodies.circle(x, y, 1,options,50,50);
       World.add(world, this.body);
     }
@@ -15,7 +17,7 @@ class Mango {
       translate(pos.x, pos.y);
       rotate(angle);
       imageMode(CENTER);
-      image(this.image, 0, 0, 30, 30);
+      image(this.image , 0, 0, this.widht, this.height);
       pop();
     }
   };
